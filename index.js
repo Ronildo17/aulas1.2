67d5e95e54fd6208be74ba839741593e384c4248
Ronildo Dias <ronildodiasr7@gmail.com>
2021-10-13 17:44:06 -0300

    salva alteraçoes

---
+++ index.js
@@ -0,0 +1,23 @@
+const aluno = 'Levi'
+const notaAluno = 9.5
+
+const aluno2 = 'Ravi'
+const notaAluno2 = 7.4
+
+const aluno3 = 'Fabi'
+const notaAluno3 = 3.5
+
+
+const media = (notaAluno + notaAluno2 + notaAluno3)/3
+
+if (media > 5) {
+    console.log(`A media foi de ${media}. parabens`)
+} else {
+    console.log('Sua media foi abaixo de 5');
+}
+
+
+
+
+
+
