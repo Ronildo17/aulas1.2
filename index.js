4ec535820d99aa79635502d8d62b23ad9b2ab963
Ronildo Dias <ronildodiasr7@gmail.com>
2021-10-13 20:36:10 -0300

    salva alteraçoes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    // Desafio//
+    // Desafio 1//
 const nome = 'Manoel'
 const peso = 104
 const altura = 1.88
@@ -12,6 +12,24 @@ if (imc >= 30 ){
     console.log('Manoel você não está acima do peso');
 }
 
+    
+//const nome = 'Silvana'
+//const sexo = 'F'
+//const idade = 48
+//const contribuicao = 23
 
+//const nome = 'Ademir'
+//const sexo = 'M'
+//const idade = 45
+//const contribuicao = 20
 
+//const calculoContribuicao = idade + contribuicao
 
+//const homemPodeAposentar = sexo == 'M' && contribuicao >= 35 && calculoContribuicao >= 95
+//const mulherPodeAposentar = sexo == 'F' && contribuicao >= 30 && calculoContribuicao >= 85
+
+//if ( homemPodeAposentar || mulherPodeAposentar) {
+//    console.log(`${nome}, você pode se aposentar!`)
+//} else {
+//    console.log(`${nome}, você não pode se aposentar!`)
+//}
